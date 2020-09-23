3d9c909469a256ec3d5702a5579ed6e4e6793650
matheusct3@gmail.com <luanlary.bg@gmail.com>
2020-09-23 03:51:49 -0300

    Criacao das rotas de login

---
+++ QuickBuy.web/ClientApp/src/app/autorizacao/guarda.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GuardaRotas = void 0;
+var GuardaRotas = /** @class */ (function () {
+    function GuardaRotas() {
+    }
+    return GuardaRotas;
+}());
+exports.GuardaRotas = GuardaRotas;
+//# sourceMappingURL=guarda.js.map
--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../Modelo/usuario";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -11,13 +12,14 @@ export class LoginComponent {
 
   public usuario;
 
-  constructor()
+  constructor(private router: Router)
   {
     this.usuario = new Usuario();
   } 
   entrar() {
     if (this.usuario.email == 'luanlary.bg@gmail.com' && this.usuario.senha == 'Sidi4124#') {
-      
+      sessionStorage.setItem("usuario-autenticado", "1");      
+      this.router.navigate(['/']);
     }
   }
 }
