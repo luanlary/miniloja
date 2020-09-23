0b1de6cbb0572eefdad24cda5ac0d5fcc05a6335
matheusct3@gmail.com <luanlary.bg@gmail.com>
2020-09-23 04:06:13 -0300

    ajuste de login e rotas

--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../Modelo/usuario";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -8,18 +8,24 @@ import { Router } from "@angular/router";
   styleUrls: ["login.component.css"],
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public usuario;
+  public returnUrl: string;
 
-  constructor(private router: Router)
+  constructor(private router: Router, private activatedRouter: ActivatedRoute)
   {
-    this.usuario = new Usuario();
+
   } 
+    ngOnInit(): void {
+      this.usuario = new Usuario();
+      this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];     
+  }
+
   entrar() {
     if (this.usuario.email == 'luanlary.bg@gmail.com' && this.usuario.senha == 'Sidi4124#') {
-      sessionStorage.setItem("usuario-autenticado", "1");      
-      this.router.navigate(['/']);
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
   }
 }
